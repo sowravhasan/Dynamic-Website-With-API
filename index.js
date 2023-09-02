3fdb660409fcd0aad195c812ce13c69bfd335f16
sowravhasan <sowrav01825611740@gmail.com>
2023-09-02 15:57:01 +0600

    minute hour added

--- index.js
+++ index.js
@@ -16,13 +16,22 @@ const handleCategory = async () =>{
         const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${cardId}`)
         const data = await response.json();
         const youtubeCard = document.getElementById('youtube-card');
+        youtubeCard.innerHTML = '';
         
         data.data.forEach((card) => {
             const div = document.createElement('div');
+            const minuteHour = card.others.posted_date;
+            fixedTime = minuteHour.slice(0,5);
+            console.log(fixedTime);
+            const hours = Math.floor(fixedTime / 3600);
+            const minutes = Math.floor((fixedTime % 3600) / 60);
             div.innerHTML = `
             <div class="">
             <div class="max-w-md w-full rounded-lg overflow-hidden shadow-lg bg-white">
-                <img src=${card.thumbnail} alt="Video Thumbnail" class="max-w-full w-full h-[200px]">
+            <div class="mb-[20px] relative">
+            <img src=${card.thumbnail} alt="Video Thumbnail" class="max-w-full w-full h-[200px]">
+            <p class="text-sm rounded absolute bottom-0 right-0 bg-black text-white p-2">${hours} hrs ${minutes} min ago</p>
+        </div>
                 <div class="flex  px-4 pt-4">
                     <img src=${card.authors[0].profile_picture} alt="Channel Logo" class="max-w-full w-10 h-10 rounded-full mr-2">
                     <h2 class="text-xl font-semibold text-gray-800 mt-[5px]">${card.title}</h2>
@@ -48,9 +57,12 @@ const handleCategory = async () =>{
 
             `
             youtubeCard.appendChild(div);
+            
+
         })
     }
 handleCategory();
+handleCard(1000);
 
 
 // const timestampInSeconds = 13885;
